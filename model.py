61d93b8d6b039c1d6d6618a25f8a1884e065156b
limlim0208 <limlimbst@gmail.com>
2025-05-26 18:43:15 +0900

    [ADD] model: Modelling

--- model.py
+++ model.py
@@ -187,3 +187,59 @@ train_generator = CustomDataGenerator(df=train,X_col='image',y_col='caption',bat
 
 validation_generator = CustomDataGenerator(df=test,X_col='image',y_col='caption',batch_size=64,directory=image_path,
                                       tokenizer=tokenizer,vocab_size=vocab_size,max_length=max_length,features=features)
+
+
+# ==============================================
+# Modelling
+# ==============================================
+
+from tensorflow.keras.utils import plot_model
+
+input1 = Input(shape=(1920,))
+input2 = Input(shape=(max_length,))
+
+img_features = Dense(256, activation='relu')(input1)
+img_features_reshaped = Reshape((1, 256), input_shape=(256,))(img_features)
+
+sentence_features = Embedding(vocab_size, 256, mask_zero=False)(input2)
+merged = concatenate([img_features_reshaped,sentence_features],axis=1)
+sentence_features = LSTM(256)(merged)
+x = Dropout(0.5)(sentence_features)
+x = add([x, img_features])
+x = Dense(128, activation='relu')(x)
+x = Dropout(0.5)(x)
+output = Dense(vocab_size, activation='softmax')(x)
+
+caption_model = Model(inputs=[input1,input2], outputs=output)
+caption_model.compile(loss='categorical_crossentropy',optimizer='adam')
+
+
+from tensorflow.keras.callbacks import ModelCheckpoint
+
+# Define the model checkpoint
+model_name = 'model.keras'  # Update the extension to .keras
+checkpoint = ModelCheckpoint(
+    model_name,
+    monitor='val_loss',
+    mode='min',
+    save_best_only=True,
+    save_weights_only=False,
+    verbose=1
+)
+
+earlystopping = EarlyStopping(monitor='val_loss',min_delta = 0, patience = 5, verbose = 1, restore_best_weights=True)
+
+learning_rate_reduction = ReduceLROnPlateau(monitor='val_loss',
+                                            patience=3,
+                                            verbose=1,
+                                            factor=0.2,
+                                            min_lr=0.00000001)
+
+
+
+
+history = caption_model.fit(
+        train_generator,
+        epochs=50,
+        validation_data=validation_generator,
+        callbacks=[checkpoint,earlystopping,learning_rate_reduction])
