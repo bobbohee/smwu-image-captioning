c33a2f6bdb7c091aa288f7fcebd1d5a9a2c1e774
yumni-song <ippunsong2@sookmyung.ac.kr>
2025-05-26 18:34:06 +0900

    [ADD] model: Image Feature Extraction

--- model.py
+++ model.py
@@ -105,3 +105,20 @@ train.reset_index(inplace=True,drop=True)
 test.reset_index(inplace=True,drop=True)
 
 print(tokenizer.texts_to_sequences([captions[1]])[0])
+
+
+# ==============================================
+# Image Feature Extraction
+# ==============================================
+model = DenseNet201()
+fe = Model(inputs=model.input, outputs=model.layers[-2].output)
+
+img_size = 224
+features = {}
+for image in tqdm(data['image'].unique().tolist()):
+    img = load_img(os.path.join(image_path,image),target_size=(img_size,img_size))
+    img = img_to_array(img)
+    img = img/255.
+    img = np.expand_dims(img,axis=0)
+    feature = fe.predict(img, verbose=0)
+    features[image] = feature
