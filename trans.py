55de6623def23d023e8fe03e4f86bba988ea94fa
bobbohee <parkbohee0705@gmail.com>
2025-06-19 20:34:31 +0900

    [MOD] model: 데이터셋 30,000개 용으로 이미지 생성 모델 코드 수정

--- trans.py
+++ trans.py
@@ -4,13 +4,7 @@ from deep_translator import GoogleTranslator
 def translate_caption():
     try:
         # 모델 캡션 생성
-        text = saved_model.generate_and_display_caption(
-            image_path='image.png', 
-            model_path='model.keras',
-            tokenizer_path='tokenizer.pkl',
-            feature_extractor_path='feature_extractor.keras',
-            max_length=34,
-            img_size=224)
+        text = saved_model.generate_and_display_caption(image_path='image.png')
 
         # 영어 -> 한국어 번역
         translator = GoogleTranslator(source='en', target='ko')
